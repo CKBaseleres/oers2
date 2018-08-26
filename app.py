33ded39150804583cc89fb16cbfc9c1352e2318c
Carl Keneth <baseleres.ck@gmail.com>
2018-08-26 22:51:17 +0800

    Added tables to Database

--- app.py
+++ app.py
@@ -149,6 +149,34 @@ class Admin(db.Model):
     def __repr__(self):
         return f"'{self.username}','{self.password}'"
 
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    fieldOfStudy = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"'{self.name}','{self.fieldOfStudy}'"
+
+class Organization(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"'{self.name}'"
+
+class Purpose(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"'{self.name}'"
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"'{self.name}'"
 
 
 ######################## FORMS ############################
